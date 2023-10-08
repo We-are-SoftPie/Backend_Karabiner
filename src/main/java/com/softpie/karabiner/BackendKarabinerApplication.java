6e4dfcbc36aa89eee5130774c034cd9c1c219696
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-10-08 19:52:29 +0900

    init

---
+++ src/main/java/com/softpie/karabiner/BackendKarabinerApplication.java
@@ -0,0 +1,13 @@
+package com.softpie.karabiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKarabinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendKarabinerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softpie/karabiner/BackendKarabinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softpie.karabiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKarabinerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
